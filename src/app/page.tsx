b7f45ad00fc8f29e9c019e5a13edce31007353c5
Oleksandr Burlachenko <oleksandr.burlachenko.ext@qonto.com>
2024-01-23 19:58:07 +0200

    test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>Test 444</p>
+        <p>Test 555</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
